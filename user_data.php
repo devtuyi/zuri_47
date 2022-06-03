4d0dfb538e55fffa79e0f955cd793894a670ce50
Oluwatuyi Segun <63879692+soluwatuyi@users.noreply.github.com>
2022-06-03 12:24:55 +0100

    Used fputcsv

--- user_data.php
+++ user_data.php
@@ -1,8 +1,16 @@
 <?php
-$file = fopen("userdata.csv", "w");
-foreach($_POST as $name => $value) {
-    fwrite($file, ucfirst($name) . ": " . htmlentities($value) . "\n");
+if(isset($_POST["submit"])) {
+    $_POST = array_map("htmlentities", $_POST);
+    $name = (string) $_POST["name"];
+    $email = (string) $_POST["email"];
+    $dob = (string) $_POST["dob"];
+    $gender = (string) $_POST["gender"];
+    $country = (string) $_POST["country"];
+    $file = fopen("userdata.csv", "w");
+    fputcsv($file, [$name, $email, $dob, $gender, $country]);
+    fclose($file);
+    print_r(file("userdata.csv"));
+} else {
+    header("Location: ./index.html");
 }
-fclose($file);
-print_r(file("userdata.csv"));
 ?>
